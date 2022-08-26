c089697d7d384be7bad47fec3f04a08a65ae2c8d
SRV1030 <shahsourabh11@gmail.com>
2022-08-26 11:55:32 +0530

    Download file Integration

--- src/pages/resource/[address]/index.js
+++ src/pages/resource/[address]/index.js
@@ -4,36 +4,69 @@ import SideNavLayout from "../../../styleGuide/layout/sidenav";
 import DisplayCard from '../../../styleGuide/components/displayCard';
 import axios from 'axios';
 import styles from "./Resource.module.css";
-
-const Resource = (props) => {
-    const {address} = props;
-    console.log(address,"dataid");
+import web3 from "../../../../ethereum/web3";
+import Resource from "../../../../ethereum/resource";
+import Link from 'next/link';
+const ResourceData = (props) => {    
+    // console.log(props.address);
+    console.log(props.res);
+    const resource=props.res;
+    const address= resource.intiId.hash;
     const [subscriptionRate,setSubscriptionRate] = useState(200);
     const [subscriberCount,setSubscriberCount] = useState(150);
     const [viewCount,setViewCount] = useState(2800);
-    const [resource,setResource] = useState();
+    const url = "https://sabhyaserver101.infura-ipfs.io/ipfs/"
   
-    useEffect(async()=>{
-      try{
-          var config = {
-              method: 'get',
-              url: `https://gentle-lowlands-02621.herokuapp.com/hei/content/${address}`,
-              headers: { 
-                'auth': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMDI2MDUzMTdiNzZmNDJjODA2NjIwNyIsImlhdCI6MTY2MTEwMDExNn0.70C7CzNfye7xpct5KoLbuNEHWCzOIPEK-MDGs5cnnOI', 
-                'type': 'institute'
-              }
-            };
+  //   useEffect(async()=>{
+  //     try{
+  //         // var config = {
+  //         //     method: 'get',
+  //         //     url: `https://gentle-lowlands-02621.herokuapp.com/hei/content/${address}`,
+  //         //     headers: { 
+  //         //       'auth': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMDI2MDUzMTdiNzZmNDJjODA2NjIwNyIsImlhdCI6MTY2MTEwMDExNn0.70C7CzNfye7xpct5KoLbuNEHWCzOIPEK-MDGs5cnnOI', 
+  //         //       'type': 'institute'
+  //         //     }
+  //         //   };
 
-          const res = await axios(config);
-          console.log(res); 
-          setResource(res.data);
+  //         // const res = await axios(config);
+  //         // console.log(res); 
+  //         // setResource(res.data);
 
-      }catch(err){
-          console.log(err,"error is error")
-      }
+  //     }catch(err){
+  //         console.log(err,"error is error")
+  //     }
 
-  },[]);
-
+  // },[]);
+  const fileHashDownload  = async (event) => {
+    event.preventDefault();
+    console.log(resource,"resource data");
+    console.log(address,"address");
+    try{
+        console.log("button clicked")
+        // const campaign = Resource(address);
+        // const accounts = await web3.eth.getAccounts();
+        // console.log(campaign, "campaign");
+        // const wei=Number(resource?.minumunContribution+1).toString();
+        // const hash1=await campaign.methods.contributeAndDownloadFile(resource.fileIdx).send({
+        //     from: accounts[0],
+        //     value: wei,
+        // });
+        // console.log(hash1,"hash1");
+        // console.log(resource.file,"my hash");
+        //   fetch(url+resource.file).then(function(t) {
+        //       return t.blob().then((b)=>{
+        //           var a = document.createElement("a");
+        //           a.href = URL.createObjectURL(b);
+        //           a.setAttribute("download", filename);
+        //           a.click();
+        //       }
+        //       );
+        //   });
+          
+    }catch(e){
+        console.log(e,"error from download")
+    }
+}
   return (
         <SideNavLayout> 
          <nav className={`${styles.nav}`}>
@@ -81,10 +114,12 @@ const Resource = (props) => {
             <p>{resource?.desc}</p>
          </div>
          <div className={`${styles.download_align}`} >
-         <button type="button" class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
+         <a href={url+resource.file} target='_blank'>
+         <button  type="button" class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
          <Icon icon="ep:download" class="mx-5" height={15} />
           Download
-         </button>
+         </button>         
+         </a>
          </div>
         </SideNavLayout>
   );
@@ -97,17 +132,27 @@ export async function getServerSideProps(context) {
   try {
       const address = context.params.address;
 
-      
+      var config = {
+        method: 'get',
+        url: `https://gentle-lowlands-02621.herokuapp.com/hei/content/${address}`,
+        headers: { 
+          'auth': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMDI2MDUzMTdiNzZmNDJjODA2NjIwNyIsImlhdCI6MTY2MTEwMDExNn0.70C7CzNfye7xpct5KoLbuNEHWCzOIPEK-MDGs5cnnOI', 
+          'type': 'institute'
+        }
+      };
+
+    const res = await axios(config);
+    console.log(res.data); 
       
 
       return {
-          props: { address:address } // ca = Contract Address;
+          props: { address:address , res:res.data } 
       }
   } catch (err) {
-      return { props: err };
+      return { props: err }
   }
 
 
 }
 
-export default Resource
+export default ResourceData;
